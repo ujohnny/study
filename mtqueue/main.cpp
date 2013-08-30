d7c23ffaf61d2903dc72997a7883b22114bfd68a
Eugeny Novozhilov <eugeny.novozhilov@gmail.com>
2013-08-30 16:31:04 +0400

    node and iterator->holder moved to queue class

--- mtqueue/main.cpp
+++ mtqueue/main.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-
+#include <cstdlib>
 #include "queue.hpp"
 
 
 int main() {
 	Queue<int> q;
-
+	srand(time(0));
+	int i = 0;
 	auto f1 = [&]() {
-		Queue<int>::iterator c = q.pop();
-		std::cout << *c << std::endl;
+	/*
+		while (i < 100) {
+			int value = rand() % 2000;
+			q.push(value);
+			std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		}
+		*/
+		int value = 2000;
+		q.push(value);
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		value = 1000;
+		q.push(value);
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		value = 1500;
+		q.push(value);
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
 	};
 
 	auto f2 = [&]() {
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		q.push(5);
+		Queue<int>::iterator c = q.pop();
+		std::cout << "1 " << *c << std::endl;
+		c = q.pop();
+		std::cout << "1 " << *c << std::endl;
+		/*int value = *c;
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		if (i >= 100)
+			return;
+			*/
 	};
-
+	
+	auto f3 = [&]() {
+		Queue<int>::iterator c = q.pop();
+		std::cout << "2 " << *c << std::endl;
+		
+		/*int value = *c;
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		if (i >= 100)
+			return;
+			*/
+	};
+	
+/*	
+	auto f3 = [&]() {
+		Queue<int>::iterator c = q.pop();
+		std::cout << *c << std::endl;
+		int value = *c;
+		std::this_thread::sleep_for(std::chrono::milliseconds(value));
+		if (i >= 100)
+			return;
+	};
+*/
 
 	std::thread t1(f1);
 	std::thread t2(f2);
+	std::thread t3(f3);
 	t1.join();
 	t2.join();
+	t3.join();
 
 }
--- mtqueue/queue.hpp
+++ mtqueue/queue.hpp
@@ -2,28 +2,6 @@
 #include <mutex>
 #include <condition_variable>
 
-template<typename V, typename N>
-struct queue_iterator {
-	N *current;
-	queue_iterator(N *n) : current(n) {}
-	~queue_iterator() { delete current; }
-	V& operator*() {
-		return current->value;
-	}
-};
-
-template <typename T>
-struct Node {
-		T value;
-		Node *next;
-		
-		template <typename A1>
-		Node(const A1& arg1, Node* n) : value(arg1), next(n) {}
-		
-		template <typename A1, typename A2>
-		Node(const A1& arg1, const A2& arg2, Node *n) : value (arg1, arg2), next(n) {}
-};
-
 // assume that type T doesn't have a copy ctor and operator new()
 template <typename T>
 class Queue {
@@ -34,20 +12,46 @@ public:
 
 	template<typename A1>
 	void push(const A1& arg1) {
-		Node<T> *n = new Node<T>(arg1, NULL);
+		Node *n = new Node(arg1);
 		push_back(n);
 	}
 		
 	template<typename A1, typename A2>
 	void push(const A1& arg1, const A2& arg2) {
-		Node<T> *n = new Node<T>(arg1, arg2, NULL);
+		Node *n = new Node(arg1, arg2);
 		push_back(n);
 	}
 	
-	queue_iterator<T, Node<T> > pop() {
+private:
+	struct Node {
+		T value;
+		Node *next;
+		
+		template <typename A1>
+		Node(const A1& arg1) : value(arg1), next(nullptr) {}
+		
+		template <typename A1, typename A2>
+		Node(const A1& arg1, const A2& arg2) : value (arg1, arg2), next(nullptr) {}
+	};
+	
+	template<typename V, typename N>
+	struct node_holder {
+	N *current;
+	node_holder(N *n) : current(n) {}
+	~node_holder() { delete current; }
+	V& operator*() {
+		return current->value;
+	}
+};
+
+public:
+	typedef node_holder<T, Node > iterator;
+	typedef node_holder<T const, Node const> const_iterator;
+
+	node_holder<T, Node> pop() {
 		std::unique_lock<std::mutex> lock(mutex_);
 		if (head_ == nullptr) {
-			cv_.wait(lock);
+			cv_.wait(lock, [&]{ return head_ != nullptr; });
 		}
 		iterator res(head_);
 		head_ = head_->next;
@@ -56,11 +60,10 @@ public:
 		}
 		return res; //rvo?
 	}
-	typedef queue_iterator<T, Node<T> > iterator;
-	typedef queue_iterator<T const, Node<T> const> const_iterator;
+
 	
 private:
-	void push_back(Node<T>* n) {
+	void push_back(Node* n) {
 		std::unique_lock<std::mutex> lock(mutex_);
 		if (head_ == nullptr) {
 			head_ = n;
@@ -72,8 +75,8 @@ private:
 		}
 	}
 	
-	Node<T> *head_;
-	Node<T> *tail_;
+	Node *head_;
+	Node *tail_;
 
 	std::mutex mutex_;
 	std::condition_variable cv_;
